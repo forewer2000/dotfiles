d01e151d05864301f0f591929bb4c6906f51e7f8
Endre Nagy <nagyendre@reea.net>
2013-02-18 17:11:09 +0200

    Finishing samba installer

--- samba_installer.sh
+++ samba_installer.sh
@@ -2,12 +2,31 @@
 
 set -e
 
-if [ "$#" != "1" ] ; then
-    echo "Usage: samba_installer.sh sambauser"
+if [ "$#" != "3" ] ; then
+    echo "Usage: samba_installer.sh sambauser workgroup netbiosname"
     exit 1
 fi
 
+sambauser=$1
+workgroup=$2
+netbios_name=$3
+
 packages_to_install=('libcupsys2' 'samba' 'samba-common' 'smbclient')
+
+mkdir "/home/$sambauser/devel" || {
+    echo "Cannot create directory /home/$sambauser/devel or already exists"
+}
+
+chown $sambauser:www-data /home/$sambauser/devel || {
+    echo "Cannot set permission to $sambauser:www-data"
+    exit 1
+}
+
+chmod 0774 /home/$sambauser/devel || {
+    echo "Cannot change owner for /home/$sambauser/devel"
+    exit 1
+}
+
 
 # install all packages
 
@@ -19,16 +38,35 @@ do
     }
 done
 
-read -r -d '' scf <<"EOF"
-[accounts]
-comment = Accounts data directory
-path = /data/accounts
-valid users = $sambauser
-public = no
-writable = yes
+
+! read -r -d '' scf <<EOF
+[global]
+socket options = TCP_NODELAY IPTOS_LOWDELAY
+workgroup = $workgroup
+netbios name = $netbios_name
+server string = %h server (Samba %v)
+log file = /var/log/samba/log.%m
+max log size = 1000
+syslog = 0
+security = user
+guest account = www-data
+unix extensions = no
+follow symlinks = yes
+wide links = yes
+client lanman auth = Yes
+lanman auth = Yes
+
+[devel]
+force group = www-data
+path=/home/$sambauser/devel
+browseable=yes
+writeable=yes
+guest ok = No
+
 EOF
 
-echo "CONF:"
-echo "$scf"
+echo "$scf" > /etc/samba/smb.conf
 
 smbpasswd -a $sambauser 
+
+service smbd restart
